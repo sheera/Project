3182bff21de8e5503568c29d23381e66798cc6d4
Sheera Adar <sheeradar@gmail.com>
2012-11-29 14:22:16 -0500

    Handled cases of no arguments

--- mean.py
+++ mean.py
@@ -1,7 +1,12 @@
+# Print the mean of numbers given as argument
+# mean.py
 import sys
-sum = 
+sum = 0
+if len(sys.argv) == 1:
+	print 'Error : No arguments give.'
+	sys.exit()
 
 for num in sys.argv[1:]:
-	sum += float(num)
+    sum += float(num)
+print sum / (len(sys.argv) - 1)
 
-print sum / (len(sys,argv) -1)
