5eedb7c9f66a929ee7c38da6ad0cc37d4f09aa83
Sheera Adar <sheeradar@gmail.com>
2012-11-29 14:57:42 -0500

    Changed to calculate mean of input file

--- mean.py
+++ mean.py
@@ -2,14 +2,19 @@
 #comment2
 #comment3
 # Print the mean of numbers given as argument
+# edited to calculate mean of numbers in  file given as an argumen
 # mean.py
 import sys
 sum = 0
+n=0
+
 if len(sys.argv) == 1:
 	print 'Error : No arguments give.'
 	sys.exit()
 
-for num in sys.argv[1:]:
+for num in open(sys.argv[1]):
     sum += float(num)
-print sum / (len(sys.argv) - 1)
+    n +=1
+
+print sum / n
 
